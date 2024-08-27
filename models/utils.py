d250cbcd353644017b6cde5f997b7ad51ea2a640
CezarFumio <65252748+CezarFumio@users.noreply.github.com>
2024-08-27 18:09:05 +0100

    Update utils.py
    comment

--- models/utils.py
+++ models/utils.py
@@ -6,13 +6,14 @@ import torch
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LambdaLR
 
-from fairseq.models.wav2vec import Wav2Vec2Model
+#from fairseq.models.wav2vec import Wav2Vec2Model
 
 
 def load_pretrained_wav2vec(ckpt_path):
     """Load pretrained Wav2Vec model."""
     ckpt = torch.load(ckpt_path)
-    model = Wav2Vec2Model.build_model(ckpt["args"], task=None)
+    exit()
+    #model = Wav2Vec2Model.build_model(ckpt["args"], task=None)
     model.load_state_dict(ckpt["model"])
     model.remove_pretraining_modules()
     model.eval()
